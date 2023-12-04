f9be20dfbe9eb0c9f77792220b5be1d035ec4d43
psyGamer <psygamer21@gmail.com>
2023-12-04 22:02:59 +0100

    Day 1 - Part 2

--- day1.zig
+++ day1.zig
@@ -1,7 +1,8 @@
 const std = @import("std");
 
 const input = @embedFile("day1_input.txt");
-// const input = @embedFile("day1_example.txt");
+// const input = @embedFile("day1_example1.txt");
+// const input = @embedFile("day1_example2.txt");
 
 pub fn main() !void {
     var sum: u32 = 0;
@@ -10,17 +11,20 @@ pub fn main() !void {
         var first: u8 = std.math.maxInt(u8);
         var last: u8 = std.math.maxInt(u8);
 
-        for (line) |char| {
-            if (char >= '0' and char <= '9') {
+        for (line, 0..) |char, i| {
+            var number: u8 = char - '0';
+            if (isSpelledNumber(line[i..])) |n| {
+                number = n;
+            }
+
+            if (number >= 0 and number <= 9) {
                 if (first == std.math.maxInt(u8)) {
                     // First number
-                    std.debug.print("{c} ", .{char});
-                    first = char - '0';
+                    first = number;
                     last = first;
                 } else {
                     // Second number
-                    last = char - '0';
-                    std.debug.print("{c} ", .{char});
+                    last = number;
                 }
             }
         }
@@ -29,4 +33,27 @@ pub fn main() !void {
         sum += value;
     }
     std.debug.print("{}\n", .{sum});
+}
+
+fn isSpelledNumber(string: []const u8) ?u8 {
+    return if (std.mem.indexOf(u8, string, "one") == 0)
+        1
+    else if (std.mem.indexOf(u8, string, "two") == 0)
+        2
+    else if (std.mem.indexOf(u8, string, "three") == 0)
+        3
+    else if (std.mem.indexOf(u8, string, "four") == 0)
+        4
+    else if (std.mem.indexOf(u8, string, "five") == 0)
+        5
+    else if (std.mem.indexOf(u8, string, "six") == 0)
+        6
+    else if (std.mem.indexOf(u8, string, "seven") == 0)
+        7
+    else if (std.mem.indexOf(u8, string, "eight") == 0)
+        8
+    else if (std.mem.indexOf(u8, string, "nine") == 0)
+        9
+    else
+        null;
 }
