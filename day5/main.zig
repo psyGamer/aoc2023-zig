7be68928dcec5691f91c027c63ad27700ca434c1
psyGamer <psygamer21@gmail.com>
2023-12-08 23:16:45 +0100

    Day 5 - Part 2: "Slight" speed improvements

--- day5/main.zig
+++ day5/main.zig
@@ -17,7 +17,6 @@ pub fn main() !void {
     const allocator = arena.allocator();
 
     std.log.info("Result (Part 1): {}", .{try solve(.one, input, allocator)});
-    std.log.info("Go do something else, since this will take a while lol..", .{});
     std.log.info("Result (Part 2): {}", .{try solve(.two, input, allocator)});
 }
 test "Part 1" {
@@ -43,6 +42,10 @@ const Mapping = struct {
         return result;
     }
 };
+const Range = struct {
+    start: usize,
+    len: usize,
+};
 
 fn parseMappings(allocator: Allocator, iter: *std.mem.TokenIterator(u8, .scalar)) !std.ArrayList(Mapping) {
     var mappings = std.ArrayList(Mapping).init(allocator);
@@ -59,12 +62,55 @@ fn lookupMapping(in: usize, mapping: []const Mapping) usize {
     }
     return in;
 }
+fn remapRanges(in: *std.ArrayList(Range), out: *std.ArrayList(Range), mapping: []const Mapping) !void {
+    for (mapping) |map| {
+        var i: usize = 0;
+        while (i < in.items.len) {
+            const range = in.items[i];
+
+            if (range.start + range.len < map.src or range.start >= map.src + map.len) {
+                i += 1;
+                continue;
+            }
+
+            const extends_after = range.start + range.len > map.src + map.len;
+            const extends_before = range.start < map.src;
+
+            if (!extends_after and !extends_before) {
+                try out.append(.{ .start = range.start - map.src + map.dst, .len = range.len });
+                _ = in.orderedRemove(i);
+                // Don't increment i, to iterate over the next value (which moved into i) as well.
+            } else if (!extends_after and extends_before) {
+                in.items[i] = .{ .start = range.start, .len = map.src - range.start };
+                try out.append(.{ .start = map.dst, .len = range.start + range.len - map.src });
+                i += 1;
+            } else if (extends_after and !extends_before) {
+                try out.append(.{ .start = range.start - map.src + map.dst, .len = map.src + map.len - range.start });
+                in.items[i] = .{ .start = map.src + map.len, .len = range.start - map.src + 1 };
+                i += 1;
+            } else if (extends_after and extends_before) {
+                in.items[i] = .{ .start = range.start, .len = map.src - range.start };
+                try out.append(.{ .start = map.dst, .len = map.len });
+                try in.append(.{ .start = map.src + map.len, .len = (range.start + range.len) - (map.src + map.len) });
+                i += 1;
+            } else {
+                unreachable;
+            }
+        }
+    }
+
+    // Pass all unmapped ranges onto the output
+    try out.appendSlice(in.items);
+}
 
 fn solve(part: Part, in: []const u8, allocator: Allocator) !u32 {
     var line_iter = tokenizeSca(u8, in, '\n');
 
     var seeds = std.ArrayList(u32).init(allocator);
     defer seeds.deinit();
+    var seed_ranges = std.ArrayList(Range).init(allocator);
+    defer seed_ranges.deinit();
+
     var seed_iter = splitSca(u8, line_iter.next().?["seeds: ".len..], ' ');
     if (part == .one) {
         while (seed_iter.next()) |seed| {
@@ -72,15 +118,10 @@ fn solve(part: Part, in: []const u8, allocator: Allocator) !u32 {
         }
     } else if (part == .two) {
         while (seed_iter.next()) |start_text| {
-            const length_text = seed_iter.next().?; // They're always pairs
-
-            const start = try parseInt(u32, start_text, 10);
-            const length = try parseInt(u32, length_text, 10);
-            try seeds.ensureUnusedCapacity(length);
-
-            for (0..length) |i| {
-                seeds.appendAssumeCapacity(start + @as(u32, @intCast(i)));
-            }
+            try seed_ranges.append(.{
+                .start = try parseInt(u32, start_text, 10),
+                .len = try parseInt(u32, seed_iter.next().?, 10),
+            });
         }
     }
 
@@ -100,19 +141,52 @@ fn solve(part: Part, in: []const u8, allocator: Allocator) !u32 {
     var humidity_to_location = try parseMappings(allocator, &line_iter);
     defer humidity_to_location.deinit();
 
-    var min_location: u32 = std.math.maxInt(u32);
-    for (seeds.items) |seed| {
-        const soil = lookupMapping(seed, seed_to_soil.items);
-        const fertilizer = lookupMapping(soil, soil_to_fertilizer.items);
-        const water = lookupMapping(fertilizer, fertilizer_to_water.items);
-        const light = lookupMapping(water, water_to_light.items);
-        const temperature = lookupMapping(light, light_to_temperature.items);
-        const humidity = lookupMapping(temperature, temperature_to_humidity.items);
-        const location = lookupMapping(humidity, humidity_to_location.items);
-        min_location = @min(min_location, location);
+    if (part == .one) {
+        var min_location: u32 = std.math.maxInt(u32);
+        for (seeds.items) |seed| {
+            const soil = lookupMapping(seed, seed_to_soil.items);
+            const fertilizer = lookupMapping(soil, soil_to_fertilizer.items);
+            const water = lookupMapping(fertilizer, fertilizer_to_water.items);
+            const light = lookupMapping(water, water_to_light.items);
+            const temperature = lookupMapping(light, light_to_temperature.items);
+            const humidity = lookupMapping(temperature, temperature_to_humidity.items);
+            const location = lookupMapping(humidity, humidity_to_location.items);
+            min_location = @min(min_location, location);
+        }
+        return min_location;
+    } else if (part == .two) {
+        var soil_ranges = std.ArrayList(Range).init(allocator);
+        defer soil_ranges.deinit();
+        // Lists will be swapped after each mapping-set
+
+        try remapRanges(&seed_ranges, &soil_ranges, seed_to_soil.items);
+        var fertilizer_ranges = seed_ranges;
+        fertilizer_ranges.clearRetainingCapacity();
+        try remapRanges(&soil_ranges, &fertilizer_ranges, soil_to_fertilizer.items);
+        var water_ranges = soil_ranges;
+        water_ranges.clearRetainingCapacity();
+        try remapRanges(&fertilizer_ranges, &water_ranges, fertilizer_to_water.items);
+        var light_ranges = fertilizer_ranges;
+        light_ranges.clearRetainingCapacity();
+        try remapRanges(&water_ranges, &light_ranges, water_to_light.items);
+        var temperature_ranges = water_ranges;
+        temperature_ranges.clearRetainingCapacity();
+        try remapRanges(&light_ranges, &temperature_ranges, light_to_temperature.items);
+        var humidity_ranges = light_ranges;
+        humidity_ranges.clearRetainingCapacity();
+        try remapRanges(&temperature_ranges, &humidity_ranges, temperature_to_humidity.items);
+        var location_ranges = temperature_ranges;
+        location_ranges.clearRetainingCapacity();
+        try remapRanges(&humidity_ranges, &location_ranges, humidity_to_location.items);
+
+        var min_location: u32 = std.math.maxInt(u32);
+        for (location_ranges.items) |range| {
+            min_location = @min(min_location, range.start);
+        }
+        return min_location;
     }
 
-    return min_location;
+    unreachable;
 }
 
 // Useful stdlib functions
