96e40dba5c10ccdc205491dfae65c868b742fc00
psyGamer <psygamer21@gmail.com>
2023-12-23 15:22:20 +0100

    Day 23: Re-added Part 1

--- day23/main.zig
+++ day23/main.zig
@@ -21,14 +21,14 @@ pub fn main() !void {
     defer arena.deinit();
     const allocator = arena.allocator();
 
-    // std.log.info("Result (Part 1): {}", .{try solve(.one, input, allocator)});
+    std.log.info("Result (Part 1): {}", .{try solve(.one, input, allocator)});
     std.log.info("Result (Part 2): {}", .{try solve2(.two, input, allocator)});
 }
 test "Part 1" {
-    try std.testing.expectEqual(@as(u64, 2), try solve2(.one, example1, std.testing.allocator));
+    try std.testing.expectEqual(@as(u64, 94), try solve(.one, example1, std.testing.allocator));
 }
 test "Part 2" {
-    // try std.testing.expectEqual(@as(u64, 6), try solve(.two, example2, std.testing.allocator));
+    try std.testing.expectEqual(@as(u64, 154), try solve2(.one, example1, std.testing.allocator));
 }
 
 const Dir = enum { l, r, u, d };
@@ -357,198 +357,145 @@ pub fn solve(comptime part: Part, in: []const u8, allocator: Allocator) !u64 {
 
     // return subsolve(.{ .x = 1, .y = 0, .dir = .d }, &memo, &visited, 0, in, width);
 
-    var map = try Array2D(u16).initWithDefault(allocator, width, height, 0);
-    defer map.deinit(allocator);
-
-    var queue = std.ArrayList(Node2).init(allocator);
-    defer queue.deinit();
-    // var queue = std.PriorityQueue(Node2, void, Node2.compare).init(allocator, {});
-    // defer queue.deinit();
-
     // try queue.add(.{ .x = @intCast(width - 2), .y = @intCast(height - 1), .dir = .u, .dist = 1000 });
-    try queue.append(.{ .x = @intCast(width - 2), .y = @intCast(height - 1), .dir = .u, .dist = 1000 });
-
-    var next = std.ArrayList(Node2).init(allocator);
-    defer next.deinit();
-
-    var best: u32 = 0;
-
-    var i: usize = 0;
-    outer: while (queue.popOrNull()) |curr_node| : (i += 1) {
-        // outer: while (queue.removeOrNull()) |curr_node| : (i += 1) {
-        var curr = curr_node;
-        map.set(curr.x, curr.y, curr.dist);
-        std.log.warn("curr {} {} {}", .{ curr, width, height });
-
-        while (true) {
-            std.log.warn("    next {} ", .{curr});
-            if (map.get(curr.x, curr.y) == 0) map.set(curr.x, curr.y, 9999 - @as(u16, @intCast(i)));
-            if (curr.y == height - 1 and curr.dir == .d) continue :outer;
-            if (curr.y == 0 and curr.dir == .u) {
-                std.log.warn("NEW BEST {}", .{curr.dist});
-                best = @max(best, curr.dist);
-                continue :outer;
-            }
-            if (curr.dir != .l and getAtPos(curr.x + 1, curr.y, width + 1, in) != '#' and map.get(curr.x + 1, curr.y) == 0) try next.append(.{ .x = curr.x + 1, .y = curr.y, .dir = .r, .dist = curr.dist + 1 });
-            if (curr.dir != .r and getAtPos(curr.x - 1, curr.y, width + 1, in) != '#' and map.get(curr.x - 1, curr.y) == 0) try next.append(.{ .x = curr.x - 1, .y = curr.y, .dir = .l, .dist = curr.dist + 1 });
-            if (curr.dir != .u and getAtPos(curr.x, curr.y + 1, width + 1, in) != '#' and map.get(curr.x, curr.y + 1) == 0) try next.append(.{ .x = curr.x, .y = curr.y + 1, .dir = .d, .dist = curr.dist + 1 });
-            if (curr.dir != .d and getAtPos(curr.x, curr.y - 1, width + 1, in) != '#' and map.get(curr.x, curr.y - 1) == 0) try next.append(.{ .x = curr.x, .y = curr.y - 1, .dir = .u, .dist = curr.dist + 1 });
-
-            if (next.items.len == 1) {
-                curr = next.items[0];
-                next.clearRetainingCapacity();
-            } else {
-                break;
-            }
-        }
-
-        std.log.warn("    next => {any} ", .{next.items});
-        try queue.appendSlice(next.items);
-        // try queue.insertSlice(0, next.items);
-        // try queue.addSlice(next.items);
-        next.clearRetainingCapacity();
-    }
-
-    std.log.warn("\n{d:0>3}", .{map});
-    return best;
 
     // var line_iter = tokenizeSca(u8, in, '\n');
     // while (line_iter.next()) |line| {
     //     _ = line;
     // }
     // const start: Node = .{ .x = 1, .y = 0, .dir = .d, .g = 0, .f = 0, .visited = std.AutoHashMap(Vec2u, void).init(allocator) };
-    // const start: Node = .{ .x = 1, .y = 0, .dir = .d, .g = 0, .f = 0, .parent = null };
-    // const end: Vec2u = .{ .x = @intCast(width - 2), .y = @intCast(height - 1) };
+    const start: Node = .{ .x = 1, .y = 0, .dir = .d, .g = 0, .f = 0, .parent = null };
+    const end: Vec2u = .{ .x = @intCast(width - 2), .y = @intCast(height - 1) };
 
-    // var goal_dist: u32 = 0;
+    var goal_dist: u32 = 0;
 
-    // var open_set = std.AutoHashMap(NodeState, u32).init(allocator);
-    // defer open_set.deinit();
-    // var closed_set = std.AutoHashMap(NodeState, u32).init(allocator);
-    // defer closed_set.deinit();
+    var open_set = std.AutoHashMap(NodeState, u32).init(allocator);
+    defer open_set.deinit();
+    var closed_set = std.AutoHashMap(NodeState, u32).init(allocator);
+    defer closed_set.deinit();
 
-    // var queue = std.PriorityQueue(Node, void, Node.compare).init(allocator, {});
-    // defer queue.deinit();
+    var queue = std.PriorityQueue(Node, void, Node.compare).init(allocator, {});
+    defer queue.deinit();
 
-    // var visited = std.AutoHashMap(Node, Vec2u).init(allocator);
-    // defer visited.deinit();
+    var visited = std.AutoHashMap(Node, Vec2u).init(allocator);
+    defer visited.deinit();
 
-    // try queue.add(start);
+    try queue.add(start);
 
-    // search: while (queue.removeOrNull()) |curr| {
-    //     std.debug.assert(!(curr.x == 0 and curr.dir != .r));
-    //     std.debug.assert(!(curr.y == 0 and curr.dir != .d));
-    //     std.debug.assert(!(curr.x == width - 1 and curr.dir != .l));
-    //     std.debug.assert(!(curr.y == height - 1 and curr.dir != .u));
+    search: while (queue.removeOrNull()) |curr| {
+        std.debug.assert(!(curr.x == 0 and curr.dir != .r));
+        std.debug.assert(!(curr.y == 0 and curr.dir != .d));
+        std.debug.assert(!(curr.x == width - 1 and curr.dir != .l));
+        std.debug.assert(!(curr.y == height - 1 and curr.dir != .u));
 
-    //     // std.log.warn("curr {}", .{curr});
+        // std.log.warn("curr {}", .{curr});
 
-    //     const tile = getAtPos(curr.x, curr.y, width + 1, in);
-    //     const neighbours: [3]?Node = switch (tile) {
-    //         // '>' => .{ if (curr.dir == .l) null else .{ .x = curr.x + 1, .y = curr.y, .dir = .r, .g = undefined, .f = undefined }, null, null },
-    //         // '<' => .{ if (curr.dir == .r) null else .{ .x = curr.x - 1, .y = curr.y, .dir = .l, .g = undefined, .f = undefined }, null, null },
-    //         // 'v' => .{ if (curr.dir == .u) null else .{ .x = curr.x, .y = curr.y + 1, .dir = .d, .g = undefined, .f = undefined }, null, null },
-    //         // '^' => .{ if (curr.dir == .d) null else .{ .x = curr.x, .y = curr.y - 1, .dir = .u, .g = undefined, .f = undefined }, null, null },
-    //         else => switch (curr.dir) {
-    //             .r => .{
-    //                 if (getAtPos(curr.x + 1, curr.y, width + 1, in) == '#') null else .{ .x = curr.x + 1, .y = curr.y, .dir = .r, .g = undefined, .f = undefined },
-    //                 if (getAtPos(curr.x, curr.y + 1, width + 1, in) == '#') null else .{ .x = curr.x, .y = curr.y + 1, .dir = .d, .g = undefined, .f = undefined },
-    //                 if (getAtPos(curr.x, curr.y - 1, width + 1, in) == '#') null else .{ .x = curr.x, .y = curr.y - 1, .dir = .u, .g = undefined, .f = undefined },
-    //             },
-    //             .l => .{
-    //                 if (getAtPos(curr.x - 1, curr.y, width + 1, in) == '#') null else .{ .x = curr.x - 1, .y = curr.y, .dir = .l, .g = undefined, .f = undefined },
-    //                 if (getAtPos(curr.x, curr.y + 1, width + 1, in) == '#') null else .{ .x = curr.x, .y = curr.y + 1, .dir = .d, .g = undefined, .f = undefined },
-    //                 if (getAtPos(curr.x, curr.y - 1, width + 1, in) == '#') null else .{ .x = curr.x, .y = curr.y - 1, .dir = .u, .g = undefined, .f = undefined },
-    //             },
-    //             .u => .{
-    //                 if (getAtPos(curr.x + 1, curr.y, width + 1, in) == '#') null else .{ .x = curr.x + 1, .y = curr.y, .dir = .r, .g = undefined, .f = undefined },
-    //                 if (getAtPos(curr.x - 1, curr.y, width + 1, in) == '#') null else .{ .x = curr.x - 1, .y = curr.y, .dir = .l, .g = undefined, .f = undefined },
-    //                 if (getAtPos(curr.x, curr.y - 1, width + 1, in) == '#') null else .{ .x = curr.x, .y = curr.y - 1, .dir = .u, .g = undefined, .f = undefined },
-    //             },
-    //             .d => .{
-    //                 if (getAtPos(curr.x + 1, curr.y, width + 1, in) == '#') null else .{ .x = curr.x + 1, .y = curr.y, .dir = .r, .g = undefined, .f = undefined },
-    //                 if (getAtPos(curr.x - 1, curr.y, width + 1, in) == '#') null else .{ .x = curr.x - 1, .y = curr.y, .dir = .l, .g = undefined, .f = undefined },
-    //                 if (getAtPos(curr.x, curr.y + 1, width + 1, in) == '#') null else .{ .x = curr.x, .y = curr.y + 1, .dir = .d, .g = undefined, .f = undefined },
-    //             },
-    //         },
-    //     };
+        const tile = getAtPos(curr.x, curr.y, width + 1, in);
+        const neighbours: [3]?Node = switch (tile) {
+            '>' => .{ if (curr.dir == .l) null else .{ .x = curr.x + 1, .y = curr.y, .dir = .r, .g = undefined, .f = undefined }, null, null },
+            '<' => .{ if (curr.dir == .r) null else .{ .x = curr.x - 1, .y = curr.y, .dir = .l, .g = undefined, .f = undefined }, null, null },
+            'v' => .{ if (curr.dir == .u) null else .{ .x = curr.x, .y = curr.y + 1, .dir = .d, .g = undefined, .f = undefined }, null, null },
+            '^' => .{ if (curr.dir == .d) null else .{ .x = curr.x, .y = curr.y - 1, .dir = .u, .g = undefined, .f = undefined }, null, null },
+            else => switch (curr.dir) {
+                .r => .{
+                    if (getAtPos(curr.x + 1, curr.y, width + 1, in) == '#') null else .{ .x = curr.x + 1, .y = curr.y, .dir = .r, .g = undefined, .f = undefined },
+                    if (getAtPos(curr.x, curr.y + 1, width + 1, in) == '#') null else .{ .x = curr.x, .y = curr.y + 1, .dir = .d, .g = undefined, .f = undefined },
+                    if (getAtPos(curr.x, curr.y - 1, width + 1, in) == '#') null else .{ .x = curr.x, .y = curr.y - 1, .dir = .u, .g = undefined, .f = undefined },
+                },
+                .l => .{
+                    if (getAtPos(curr.x - 1, curr.y, width + 1, in) == '#') null else .{ .x = curr.x - 1, .y = curr.y, .dir = .l, .g = undefined, .f = undefined },
+                    if (getAtPos(curr.x, curr.y + 1, width + 1, in) == '#') null else .{ .x = curr.x, .y = curr.y + 1, .dir = .d, .g = undefined, .f = undefined },
+                    if (getAtPos(curr.x, curr.y - 1, width + 1, in) == '#') null else .{ .x = curr.x, .y = curr.y - 1, .dir = .u, .g = undefined, .f = undefined },
+                },
+                .u => .{
+                    if (getAtPos(curr.x + 1, curr.y, width + 1, in) == '#') null else .{ .x = curr.x + 1, .y = curr.y, .dir = .r, .g = undefined, .f = undefined },
+                    if (getAtPos(curr.x - 1, curr.y, width + 1, in) == '#') null else .{ .x = curr.x - 1, .y = curr.y, .dir = .l, .g = undefined, .f = undefined },
+                    if (getAtPos(curr.x, curr.y - 1, width + 1, in) == '#') null else .{ .x = curr.x, .y = curr.y - 1, .dir = .u, .g = undefined, .f = undefined },
+                },
+                .d => .{
+                    if (getAtPos(curr.x + 1, curr.y, width + 1, in) == '#') null else .{ .x = curr.x + 1, .y = curr.y, .dir = .r, .g = undefined, .f = undefined },
+                    if (getAtPos(curr.x - 1, curr.y, width + 1, in) == '#') null else .{ .x = curr.x - 1, .y = curr.y, .dir = .l, .g = undefined, .f = undefined },
+                    if (getAtPos(curr.x, curr.y + 1, width + 1, in) == '#') null else .{ .x = curr.x, .y = curr.y + 1, .dir = .d, .g = undefined, .f = undefined },
+                },
+            },
+        };
 
-    //     neighbours: for (&neighbours) |neighbour| {
-    //         if (neighbour == null) continue;
-    //         var neigh: Node = neighbour.?;
+        neighbours: for (&neighbours) |neighbour| {
+            if (neighbour == null) continue;
+            var neigh: Node = neighbour.?;
 
-    //         // if (neigh.visited.contains(.{ .x = neigh.x, .y = neigh.y })) continue;
+            // if (neigh.visited.contains(.{ .x = neigh.x, .y = neigh.y })) continue;
 
-    //         var c: ?*const Node = &curr;
-    //         while (c) |cc| {
-    //             // std.log.warn("{any}", .{c});
-    //             if (cc.x == neigh.x and cc.y == neigh.y) continue :neighbours;
-    //             c = cc.parent;
-    //         }
+            var c: ?*const Node = &curr;
+            while (c) |cc| {
+                // std.log.warn("{any}", .{c});
+                if (cc.x == neigh.x and cc.y == neigh.y) continue :neighbours;
+                c = cc.parent;
+            }
 
-    //         neigh.parent = try allocator.create(Node);
-    //         neigh.parent.?.* = curr;
-    //         // std.log.warn("PAR{any}", .{neigh.parent});
+            neigh.parent = try allocator.create(Node);
+            neigh.parent.?.* = curr;
+            // std.log.warn("PAR{any}", .{neigh.parent});
 
-    //         neigh.g = curr.g + 1;
-    //         const h = @abs(end.x - neigh.x) + @abs(end.y - neigh.y);
-    //         neigh.f = neigh.g + h;
-    //         // try neigh.visited.put(.{ .x = curr.x, .y = curr.y }, {});
+            neigh.g = curr.g + 1;
+            const h = @abs(end.x - neigh.x) + @abs(end.y - neigh.y);
+            neigh.f = neigh.g + h;
+            // try neigh.visited.put(.{ .x = curr.x, .y = curr.y }, {});
 
-    //         // std.log.err("    next {}", .{neigh});
+            // std.log.err("    next {}", .{neigh});
 
-    //         if (neigh.x == end.x and neigh.y == end.y) {
-    //             if (goal_dist < neigh.g) {
-    //                 goal_dist = neigh.g;
-    //                 // break :search;
-    //                 std.log.err("GOAL HIT {}", .{goal_dist});
-    //             }
-    //             // var c: ?*Node = &neigh;
-    //             // while (c != null) {
-    //             //     std.log.err("   {}", .{c.?.*});
-    //             //     c = c.?.*.parent;
-    //             // }
-    //             continue :search;
-    //         }
+            if (neigh.x == end.x and neigh.y == end.y) {
+                if (goal_dist < neigh.g) {
+                    goal_dist = neigh.g;
+                    // break :search;
+                    std.log.err("GOAL HIT {}", .{goal_dist});
+                }
+                // var c: ?*Node = &neigh;
+                // while (c != null) {
+                //     std.log.err("   {}", .{c.?.*});
+                //     c = c.?.*.parent;
+                // }
+                continue :search;
+            }
 
-    //         // if (neigh.g >= 155) {
-    //         //     std.log.err("ILLEGAL", .{});
-    //         //     var c: ?*Node = &neigh;
-    //         //     while (c != null) {
-    //         //         std.log.err("   {}", .{c.?.*});
-    //         //         c = c.?.*.parent;
-    //         //     }
-    //         //     break :search;
-    //         // }
+            // if (neigh.g >= 155) {
+            //     std.log.err("ILLEGAL", .{});
+            //     var c: ?*Node = &neigh;
+            //     while (c != null) {
+            //         std.log.err("   {}", .{c.?.*});
+            //         c = c.?.*.parent;
+            //     }
+            //     break :search;
+            // }
 
-    //         // const state: NodeState = .{ .x = neigh.x, .y = neigh.y, .dir = neigh.dir };
-    //         // const open_gop = try open_set.getOrPut(state);
-    //         // if (!open_gop.found_existing) {
-    //         //     open_gop.value_ptr.* = neigh.f;
-    //         //     try queue.add(neigh);
-    //         // } else if (open_gop.value_ptr.* < neigh.f) {
-    //         //     open_gop.value_ptr.* = neigh.f;
-    //         //     try queue.add(neigh);
-    //         // } else {
-    //         //     continue;
-    //         // }
+            // const state: NodeState = .{ .x = neigh.x, .y = neigh.y, .dir = neigh.dir };
+            // const open_gop = try open_set.getOrPut(state);
+            // if (!open_gop.found_existing) {
+            //     open_gop.value_ptr.* = neigh.f;
+            //     try queue.add(neigh);
+            // } else if (open_gop.value_ptr.* < neigh.f) {
+            //     open_gop.value_ptr.* = neigh.f;
+            //     try queue.add(neigh);
+            // } else {
+            //     continue;
+            // }
 
-    //         try queue.add(neigh);
+            try queue.add(neigh);
 
-    //         // if (closed_set.get(state)) |closed| {
-    //         //     if (closed >= neigh.f) continue;
-    //         // }
-    //     }
+            // if (closed_set.get(state)) |closed| {
+            //     if (closed >= neigh.f) continue;
+            // }
+        }
 
-    //     // try closed_set.put(.{ .x = curr.x, .y = curr.y, .dir = curr.dir }, curr.f);
-    //     // var v = @constCast(&curr.visited);
-    //     // v.deinit();
-    //     // if (curr.parent != null) allocator.destroy(curr.parent.?);
-    // }
+        // try closed_set.put(.{ .x = curr.x, .y = curr.y, .dir = curr.dir }, curr.f);
+        // var v = @constCast(&curr.visited);
+        // v.deinit();
+        // if (curr.parent != null) allocator.destroy(curr.parent.?);
+    }
 
-    // std.log.warn("GOAL {}", .{goal_dist});
+    std.log.warn("GOAL {}", .{goal_dist});
 
-    // return goal_dist;
+    return goal_dist;
 }
 
 // Useful stdlib functions
